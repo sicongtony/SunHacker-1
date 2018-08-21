bb4bb5651568bc08312d24a13cc069c83e99ba05
Boris <395720667@qq.com>
2018-08-21 19:59:06 +1000

    index page revised

--- js/index.js
+++ js/index.js
@@ -1,14 +1,20 @@
-window.onload = function(){
-  initApp();
-};
-function initApp(){
-  // firebase.auth().onAuthStateChanged(function(user){
-  //   if(user){
-  //     alert(user.email)
-  //     window.location = 'mainpage.html';
-  //   }
-  // });
-  if(firebase.auth().currentUser){
-    firebase.auth().signOut();
-  }
-}
+(function($){
+	// "use strict";
+
+	var navbarCollapse = function(){
+		var nav = $("#mainNav");
+		if(nav.length){
+			if(nav.offset().top > 100){
+				nav.addClass("navbar-shrink");
+			}else{
+				nav.removeClass("navbar-shrink");
+			}
+		}else{
+			alert("not exist")
+		}
+		
+	}
+
+	navbarCollapse();
+	$(window).scroll(navbarCollapse);
+})(jQuery);

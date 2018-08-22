c678fab1029a561d4a9da06480bd55fca80aac0d
Boris <395720667@qq.com>
2018-08-22 11:43:04 +1000

    index catch is done

---
+++ js/index.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	$.ajax({
+		type:"GET",
+		url:"https://uvdata.arpansa.gov.au/xml/uvvalues.xml",
+		dataType: "xml",
+		success:xmlParser
+	});
+});
+
+function xmlParser(xml){
+	$(xml).find('location[id="Melbourne"]').each(function(){
+		var index = $(this).find('index').text();
+		var attention;
+		if(index < 3){
+			attention = ", don't worry about uv";
+		}
+		else if(index < 5){
+			attention = ", be careful about the sun";
+		}
+		else if(index < 8){
+			attention = ", uv is quite high, please put sunscreen";
+		}
+		else{
+			attention = ",please stay in house avoiding sunburn";
+		}
+		$("#uv-index").append(index, attention);
+	})
+}
